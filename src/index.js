eba3dc6ef887381958f7cc707a3282700dc6a919
Serhii Zanoziuk <serg17062@gmail.com>
2018-05-18 17:57:47 +0300

    6

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { store } from './store';
+import { store } from './store';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-let res = '';
+
+
+const initCreateAction = store => typeStr => payload => store.dispatch({ type: typeStr, payload });
+
+const createAction = initCreateAction(store);
+
+const addValueAction = createAction('ADD_VALUE');
+const clearValueAction = createAction('CLEAR_VALUE');
+const calculateAction = createAction('CALCULATE');
 
 const numbers = getButtons('btn_num').filter(item => item.id !== 'clear');
 const actions = getButtons('btn_act');
@@ -15,60 +23,44 @@ const input = document.querySelector('.inp');
 const clear = document.getElementById('clear');
 const run = document.getElementById('run');
 
-clear.addEventListener( 'click', () => {
-  input.value = '';
-  res = '';
-});
+render( store );
+
+clear.addEventListener( 'click', () => clearValueAction() );
 
 numbers.forEach( el => {
   el.addEventListener( 'click', e => {
-    res += e.target.name;
-    input.value = res;
+    let res = e.target.name;
+    addValueAction(res);
   });
 });
 
 actions.forEach( el => {
   el.addEventListener( 'click', e => {
-    res += e.target.name;
-    input.value = res;
+    let res = e.target.name;
+    addValueAction(res);
   });
 });
 
-run.addEventListener( 'click', () => {
-  if ( res.length && !isNaN(res[0]) ) {
-    res = calculate( res );
-    input.value = res;
-  }
-});
+run.addEventListener( 'click', () => calculateAction()
 
-input.addEventListener( 'change', (e) => {
-  input.value = e.target.value;
-});
+// {
+//   if ( res.length && !isNaN(res[0]) ) {
+//     res = calculate( res );
+//     input.value = res;
+//   }
+// }
+);
+
+// input.addEventListener( 'change', (e) => {
+//   input.value = e.target.value;
+// });
+
+store.subscribe( function() { render( store ) });
+
+function render( store ) {
+  input.value = store.getState().result;
+}
 
 function getButtons( className ) {
   return Array.from( document.querySelectorAll(`.${className}`) );
-}
-
-function calculate( str ) {
-  let result = null;
-  const values = str.match(/\d+(\.\d+)?/g).map( el => parseFloat(el) );
-  const action = str.match(/[^\d.]/g);
-
-  if ( action ) {
-    if ( action[0] === '+' ) {
-      result = values[0] + values[1]
-    }
-    if ( action[0] === '-' ) {
-      result = values[0] - values[1];
-    }
-    if ( action[0] === 'x' ) {
-      result = values[0] * values[1];
-    }
-    if ( action[0] === '/' ) {
-      result = values[0] / values[1];
-    }
-    return result + '';
-  }
-  
-  return result = 'Error';
 }
--- src/store.js
+++ src/store.js
@@ -1,26 +1,54 @@
 import { createStore } from 'redux';
 
 const initialState = {
-  value: null
+  result: ''
 }
 
 export const store = createStore( function( state = initialState, action) {
   switch ( action.type ) {
     case 'ADD_VALUE':
       return {
-        //записывать результат одной строкой а потом ее разобрать regexp_ами
-        //преобразовать payload
-        value: state.value + action.payload
+        result: state.result + action.payload
       }
     case 'CALCULATE': {
+      let res = '';
+      if ( state.result.length && !isNaN(state.result[0]) ) {
+        res = calculate( state.result );
+      } else {
+        res = 'Error'
+      }
       return {
-        //разбор строки вычисление результата
+        result: res
       }
     }
-    case 'CLEAR':
+    case 'CLEAR_VALUE':
       return {
-        value: null
+        result: ''
       }
     default: return state;
   }
 });
+
+function calculate( str ) {
+  let result = null;
+  const values = str.match(/\d+(\.\d+)?/g).map( el => parseFloat(el) );
+  const action = str.match(/[^\d.]/g);
+
+  if ( action ) {
+    if ( action[0] === '+' ) {
+      result = values[0] + values[1]
+    }
+    if ( action[0] === '-' ) {
+      result = values[0] - values[1];
+    }
+    if ( action[0] === 'x' ) {
+      result = values[0] * values[1];
+    }
+    if ( action[0] === '/' ) {
+      result = values[0] / values[1];
+    }
+    return result + '';
+  }
+
+  return result = 'Error';
+}
